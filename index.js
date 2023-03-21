11b5f8f9ab77b5627d1f1813fd510d441dfca096
khawla6 <128473124+khawla6@users.noreply.github.com>
2023-03-21 13:16:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// code to handle user profile editing and password changing would go here
+// for example:
+
+document.querySelector('button.edit-profile').addEventListener('click', function() {
+	// show edit form
+});
+
+document.querySelector('button.change-password').addEventListener('click', function() {
+	// show password change form
+});
+
+document.querySelector('button.logout').addEventListener('click', function() {
+	// log the user out
+});
